96cf106370da3236e3909cf0d83c0914adf7db7c
KerolosNabil7 <kerolosnabil926@gmail.com>
2025-06-01 11:41:10 +0300

    Add ApplicationDbContext class

---
+++ In-Memory Caching/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using In_Memory_Caching.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace In_Memory_Caching.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
