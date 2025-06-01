b5db308a8eeb133dab526cb92e947825e0e56db8
KerolosNabil7 <kerolosnabil926@gmail.com>
2025-06-01 11:26:44 +0300

    Register the in-memory caching service

--- In-Memory Caching/Program.cs
+++ In-Memory Caching/Program.cs
@@ -2,6 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Register the in-memory caching service
+builder.Services.AddMemoryCache();
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
