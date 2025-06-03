74f8c075536c33f94c84a261c57682fa6da374d5
KerolosNabil7 <kerolosnabil926@gmail.com>
2025-06-03 11:30:08 +0300

    Adding minimal API Endpoints

--- In-Memory Caching/Program.cs
+++ In-Memory Caching/Program.cs
@@ -1,4 +1,5 @@
 using In_Memory_Caching.Data;
+using In_Memory_Caching.DTOs;
 using In_Memory_Caching.Services;
 using Microsoft.EntityFrameworkCore;
 
@@ -36,5 +37,26 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Minimal API Endpoints
+app.MapGet("/products", async (IProductService service) =>
+{
+    var products = await service.GetAll();
+    return Results.Ok(products);
+});
+
+app.MapGet("/products/{id:guid}", async (Guid id, IProductService service) =>
+{
+    var product = await service.Get(id);
+    return product is not null ? Results.Ok(product) : Results.NotFound();
+});
+
+app.MapPost("/products", async (ProductCreationDto product, IProductService service) =>
+{
+    await service.Add(product);
+    return Results.Created($"/products", product); // You can customize the location URI
+});
+
+
 
 app.Run();
